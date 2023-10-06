aebb42c8bbe470937f969e7f2c5e40c55abc333c
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2023-10-06 21:56:01 +0700

    6 Oktober 2023

---
+++ coding/4.php
@@ -0,0 +1,9 @@
+<?php
+$jumlah=0;
+for($i=11;$i<12345;$i++){
+    if($i%2==1){
+        $jumlah++;
+    }
+}
+echo "Jumlah cacah bilangan ganjil antara 10 dan 12345 adalah $jumlah";
+?>
---
+++ coding/4tapikedua.php
@@ -0,0 +1,4 @@
+<?php
+$lebihkecildari=12345;$lebihbesardari=10;
+echo "Jumlah cacah bilangan ganjil antara $lebihbesardari dan $lebihkecildari adalah ",($lebihkecildari-1-$lebihbesardari)/2;
+?>
