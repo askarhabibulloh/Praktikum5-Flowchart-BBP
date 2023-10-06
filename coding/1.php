71de87e02c15b961b2cd32beabf5542f4ab9c73f
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2023-10-06 21:32:52 +0700

    6 Oktober 2023

--- coding/1.php
+++ coding/1.php
@@ -1,3 +1,24 @@
 <?php
+//start
+$nim = $_POST["NIM"];
+$kehadiran = $_POST["kehadiran"];
+$tugas = $_POST["tugas"];
+$kuis = $_POST["kuis"];
+$UTS = $_POST["UTS"];
+$UAS = $_POST["UAS"];
 
+
+$nilaiakhir = $kehadiran*0.05 + $tugas*0.25 + $kuis*0.20 + $UTS*0.25 +$UAS*0.25;
+
+if($nilaiakhir>=90){
+    $grade = "A";
+}else if($nilaiakhir>=80){
+    $grade = "B";
+}else if($nilaiakhir>=60){
+    $grade = "C";
+}else{
+    $grade ="D";
+}
+
+echo "Nilai anda adalah $nilaiakhir dan grade anda $grade"
 ?>
